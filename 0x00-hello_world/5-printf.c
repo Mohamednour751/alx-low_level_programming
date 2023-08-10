d948a54a8c9dbcdadcbfbee5e69a1554f0827a38
Mohamednour751 <mohamed.nour426@icloud.com>
2023-08-10 16:45:43 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - Entry point 
+ * main - Entry point
+ * description: A C progam that prints with printf fnction
  * Return: ALways 0 (Success)
 */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
