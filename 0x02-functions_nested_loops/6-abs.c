ca7c25edfe554db48e7aa9703d222c6fe37913e6
Mohamednour751 <mohamed.nour426@icloud.com>
2023-08-15 15:12:29 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs -fnction that computes yhe absolute
+ *	 value of an integer
+ * @n: takes in integer type input for function
+ * Return: Always 0
+*/
+int _abs(int n)
+{
+	if (n < 0)
+		n = (-1) * n;
+	return (0);
+}
