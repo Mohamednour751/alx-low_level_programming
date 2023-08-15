e27f750138415d293e7562842f37a2f0fa20ae7c
Mohamednour751 <mohamed.nour426@icloud.com>
2023-08-15 10:47:56 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ * Description: using printf
+ * Return: Always 0
+*/
 int main(void)
 {
-        char str[10] = ("_putchar");
-	printf("%s\n", str);
+	char str[10] = ("_putchar");
+
+	printf("%s", str);
 return (0);
 }
