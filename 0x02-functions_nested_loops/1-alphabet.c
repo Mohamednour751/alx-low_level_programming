742d0af94844150b7d00f1c7eaedc609b0c0f456
Mohamednour751 <mohamed.nour426@icloud.com>
2023-08-15 13:02:26 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet -ulilizes on the -putchar function to print
+ * the alphabet a - z
+*/
+void print_alphabet(void);
+{
+	int ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
