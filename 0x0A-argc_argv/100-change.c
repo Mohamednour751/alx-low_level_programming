8e2169459badb5706e7964a1db3ea780b43e7328
Mohamednour751 <mohamed.nour426@icloud.com>
2023-09-04 08:29:37 -0700

    100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,11 +12,11 @@ int main(int argc, char *argv[])
 	if (argc == 2)
 	{
 		int i, leastcents = 0, money = atoi(argv[1]);
-		int cents[] = (25, 10, 5, 2, 1);
+		int cents[] = {25, 10, 5, 2, 1};
 
 		for (i = 0; i < 5; i++)
 		{
-			if (moeny >= cents[i])
+			if (money >= cents[i])
 			{
 				leastcents += money / cents[i];
 				money = money % cents[i];
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
 	else
 	{
 		printf("error\n");
-		return ();
+		return (1);
 	}
-	return ();
+	return (0);
 }
